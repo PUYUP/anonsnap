ae41d95169f77728b0bf64fd8aba89043c63e58c
PUYUP <hellopuyup@gmail.com>
2022-03-21 23:05:13 +0700

    prepare

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -11,11 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 import os
-from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-PROJECT_ROOT = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.abspath(os.path.dirname(__name__))
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
--- src/config/settings/production.py
+++ src/config/settings/production.py
@@ -20,8 +20,8 @@ DATABASES = {
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
-IMAGE_FOLDER = os.path.join(PROJECT_ROOT, 'image')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+IMAGE_FOLDER = os.path.join(BASE_DIR, 'image')
