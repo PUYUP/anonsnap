9e002f8931231c39ce01b5a4fcb26cae1b7a1608
PUYUP <hellopuyup@gmail.com>
2022-03-21 22:53:11 +0700

    prepare

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -104,11 +105,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+TIME_ZONE = 'Asia/Jakarta'
 USE_I18N = True
-
 USE_TZ = True
 
 
--- src/config/settings/production.py
+++ src/config/settings/production.py
@@ -15,3 +15,13 @@ DATABASES = {
         }
     }
 }
+
+# Static files (CSS, JavaScript, Images)
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+IMAGE_FOLDER = os.path.join(MEDIA_ROOT, 'image')
