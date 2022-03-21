f1c14e1a13e53ff65a0eacb0b8e18f75311cf27d
PUYUP <hellopuyup@gmail.com>
2022-03-21 22:39:44 +0700

    prepare

--- src/config/settings/production.py
+++ src/config/settings/production.py
@@ -1,1 +1,17 @@
 from .project import *
+
+
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'anonsnap',
+        'USER': 'anonsnap',
+        'PASSWORD': 'LQK$hRFJqHWZQy$@K6',
+        'HOST': HOST,   # Or an IP Address that your DB is hosted on
+        'PORT': '',
+        'OPTIONS': {
+            'sql_mode': 'STRICT_TRANS_TABLES',
+        }
+    }
+}
