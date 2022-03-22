6bf970d630afddf353186c76b9a998189cbd6903
PUYUP <hellopuyup@gmail.com>
2022-03-22 12:38:40 +0700

    prepare

--- src/config/wsgi.py
+++ src/config/wsgi.py
@@ -7,13 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
-from django.core.wsgi import get_wsgi_application
 import os
-import sys
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(BASE_DIR)
+from django.core.wsgi import get_wsgi_application
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'config.settings.production'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
 
 application = get_wsgi_application()
