43e6890ee6f4d62fee2a7b52c7e83c9da3b43d5a
PUYUP <hellopuyup@gmail.com>
2022-03-23 21:51:30 +0700

    prepare

--- src/apps/snap/models/base.py
+++ src/apps/snap/models/base.py
@@ -229,6 +229,7 @@ class AbstractReaction(AbstractCommonField):
 
     class Meta:
         abstract = True
+        managed = False
 
     def __str__(self):
         return self.get_identifier_display()
