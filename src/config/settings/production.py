a9328f86c05f6b300392a35994692e4161483df2
PUYUP <hellopuyup@gmail.com>
2022-03-23 22:48:51 +0700

    prepare

--- src/config/settings/production.py
+++ src/config/settings/production.py
@@ -2,7 +2,7 @@
 from .project import *
 
 ALLOWED_HOSTS = ['117.53.45.69', 'be.anonsnap.com']
-DEBUG = True
+DEBUG = False
 
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 DATABASES = {
